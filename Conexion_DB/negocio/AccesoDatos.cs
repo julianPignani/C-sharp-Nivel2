50aa88b989b4c069a8224c0ea4856dcadcb04dec
Julian Pignani <pignanijulian@gmail.com>
2023-09-27 21:00:52 -0300

    Acceso a datos - Centralizar Conexion

---
+++ Conexion_DB/negocio/AccesoDatos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace negocio
+{
+    //Clase para conectarse a la base de datos para que sea reutilisable.
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS01; database= POKEDEX_DB; integrated security = true");
+            comando = new SqlCommand();
+        }
+
+        public void setearQuery(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarQuery()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+    }
+}
